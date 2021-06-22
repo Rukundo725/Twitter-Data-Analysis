56c6861faf8f4697e7781176014ac1b28768a71a
Germain Rukundo <51451731+Rukundo725@users.noreply.github.com>
2021-06-21 23:26:11 -0700

    Update fix_clean_tweets_dataframe.py

--- fix_clean_tweets_dataframe.py
+++ fix_clean_tweets_dataframe.py
@@ -1,10 +1,9 @@
 import pandas as pd
-
+import json
+DataFrame = pd.read_json('data/covid19.json', lines=True)
 class Clean_Tweets:
     """
-   
-   
-   The PEP8 Standard AMAZING!!!
+    The PEP8 Standard AMAZING!!!
     """
     def __init__(self, df:pd.DataFrame):
         self.df = df
@@ -20,36 +19,32 @@ class Clean_Tweets:
         df = df[df['polarity'] != 'polarity']
         
         return df
+    
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
         
-        ---
+        df.drop_duplicates()
         
         return df
+    
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+        df['created_at'] = pd.to_datetime(df['created_at'])
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
         return df
-    
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.
-        
-        ----
-        ----
-        
+        df[['polarity','subjectivity','retweet_count','favorite_coun']] = df[['polarity','subjectivity','retweet_count','favorite_coun']].apply(pd.to_numeric)
+       
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
@@ -57,6 +52,6 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = ----
+#         df = ----
         
         return df
