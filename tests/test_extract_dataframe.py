789e589d458d13a7286fae5e438ce39a54cc4942
Germain Rukundo <51451731+Rukundo725@users.noreply.github.com>
2021-06-29 20:19:56 -0700

    Update test_extract_dataframe.py

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -73,14 +73,17 @@ class TestTweetDfExtractor(unittest.TestCase):
     def test_find_retweet_count(self):
         self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
 
-    # def test_find_hashtags(self):
-    #     self.assertEqual(self.df.find_hashtags(), )
+    def test_find_hashtags(self):
+        self.assertEqual(self.df.find_hashtags(),['red4research'] )
 
     # def test_find_mentions(self):
     #     self.assertEqual(self.df.find_mentions(), )
 
     def test_find_location(self):
         self.assertEqual(self.df.find_location(), ['Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
+    
+    def test_find_lang(self):
+    	self.assertEqual(self.df.find_lang(), ['en','en','en','en','en'])
 
 if __name__ == '__main__':
 	unittest.main()
